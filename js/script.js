ac0857acac29e528d59025f8b639851d8aba128a
NajooDev <vonam706@gmail.com>
2024-11-08 21:57:42 +0700

    use bootstrap to all page and add Lab 6

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let carouselWidth = $('.carousel-inner')[0].scrollWidth;
+let cardWidth = $('.carousel-item').width();
+
+let scrollPosition = 0;
+
+$('.carousel-control-next').on('click', function () {
+    if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
+        console.log('next');
+        scrollPosition = scrollPosition + cardWidth;
+        $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    }
+});
+
+$('.carousel-control-prev').on('click', function () {
+    if (scrollPosition > 0) {
+        console.log('prev');
+        scrollPosition = scrollPosition - cardWidth;
+        $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    }
+});
+
